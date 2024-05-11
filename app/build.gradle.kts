28cf5a64d2a9633520c6fb897d14433f1e724ad8
Youngmin Cho <choym2015@gmail.com>
2024-05-11 14:13:59 +0900

    Gradle clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,29 +55,32 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.2.0-alpha04")
 
-    //viewmodel
+    //View Model
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-compiler:2.7.0")
 
-    //retrofit
+    //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    //Glide image loading
+    //Layout
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.2.0-alpha04")
+
+    //Glide
     implementation("com.github.bumptech.glide:glide:4.11.0")
 
+    //Location
     implementation("com.google.android.gms:play-services-location:21.2.0")
-    implementation("androidx.lifecycle:lifecycle-compiler:2.7.0")
-    //testing
+
+    //Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.4.0")
     androidTestImplementation("androidx.test:rules:1.4.0")
     androidTestImplementation("androidx.test:runner:1.4.0")
-
 }
